10012ffdc91158cb0d88c54b718a88953961db29
Anderson Oliveira <contactandersonoliveira@gmail.com>
2021-03-29 18:30:16 -0300

    updating path of modals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,17 +14,17 @@ import { CandidateComponent } from './views/admin/candidate/candidate.component'
 import { QuestionsComponent } from './views/admin/questions/questions.component';
 import { ProfileComponent } from './views/admin/profile/profile.component';
 import { AdminCandidateQuestionsComponent } from './components/template/admin-candidate-questions/admin-candidate-questions.component';
-import { DeleteCandidateModalComponent } from './views/admin/modals/delete-candidate-modal/delete-candidate-modal.component';
-import { EditCandidateModalComponent } from './views/admin/modals/edit-candidate-modal/edit-candidate-modal.component';
-import { AddCandidateModalComponent } from './views/admin/modals/add-candidate-modal/add-candidate-modal.component';
-import { AddQuestionModalComponent } from './views/admin/modals/add-question-modal/add-question-modal.component';
-import { EditQuestionModalComponent } from './views/admin/modals/edit-question-modal/edit-question-modal.component';
-import { DeleteQuestionModalComponent } from './views/admin/modals/delete-question-modal/delete-question-modal.component';
+import { DeleteCandidateModalComponent } from './views/admin/modals/candidate/delete-candidate-modal/delete-candidate-modal.component';
+import { EditCandidateModalComponent } from './views/admin/modals/candidate/edit-candidate-modal/edit-candidate-modal.component';
+import { AddCandidateModalComponent } from './views/admin/modals/candidate/add-candidate-modal/add-candidate-modal.component';
+import { AddQuestionModalComponent } from './views/admin/modals/questions/add-question-modal/add-question-modal.component';
+import { EditQuestionModalComponent } from './views/admin/modals/questions/edit-question-modal/edit-question-modal.component';
+import { DeleteQuestionModalComponent } from './views/admin/modals/questions/delete-question-modal/delete-question-modal.component';
 import { AdminQuestionTableComponent } from './components/template/admin-question-table/admin-question-table.component';
 import { AdminProfileTableComponent } from './components/template/admin-profile-table/admin-profile-table.component';
-import { DeleteProfileModalComponent } from './views/admin/modals/delete-profile-modal/delete-profile-modal.component';
-import { EditProfileModalComponent } from './views/admin/modals/edit-profile-modal/edit-profile-modal.component';
-import { AddProfileModalComponent } from './views/admin/modals/add-profile-modal/add-profile-modal.component';
+import { DeleteProfileModalComponent } from './views/admin/modals/profiles/delete-profile-modal/delete-profile-modal.component';
+import { EditProfileModalComponent } from './views/admin/modals/profiles/edit-profile-modal/edit-profile-modal.component';
+import { AddProfileModalComponent } from './views/admin/modals/profiles/add-profile-modal/add-profile-modal.component';
 import { LoginComponent } from './views/user/login/login.component';
 import { AdminLoginComponent } from './views/admin/admin-login/admin-login.component';
 
